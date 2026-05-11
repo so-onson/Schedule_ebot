4c67f44d8fd385312a2802e9693d97f0a823131b
so-onson <samodurova-2003@mail.ru>
2026-05-11 22:52:40 +0300

    Add changes for host

--- bot.py
+++ bot.py
@@ -1,26 +1,47 @@
 import asyncio
+import os
+
+from dotenv import load_dotenv
+
 from aiogram import Bot, Dispatcher
-from config import TOKEN
+
 from handlers import router
 from db import init_db
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from scheduler import check_sessions
 
+
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+
+
 async def main():
+
     init_db()
 
     bot = Bot(token=TOKEN)
+
     dp = Dispatcher()
 
     dp.include_router(router)
 
+    scheduler = AsyncIOScheduler()
+
+    scheduler.add_job(
+        check_sessions,
+        "interval",
+        minutes=10,
+        args=[bot]
+    )
+
+    scheduler.start()
+
     print("Бот запущен")
+
     await dp.start_polling(bot)
 
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(check_sessions, "interval", minutes=10, args=[bot])
-    scheduler.start()
 
 if __name__ == "__main__":
     asyncio.run(main())
